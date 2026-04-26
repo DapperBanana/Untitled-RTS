1ab705434f9bf171f2074152c58b04d3ca8c8022
Austin L. Howard <contact@austinlhoward.com>
2026-04-25 18:32:00 -0600

    Create minimap unit icon scene

---
+++ scripts/minimap_unit_icon.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+@export var unit = null
+@export var building = null
+@export var minimap = null
+
+func _process(delta):
+	if unit != null:
+		global_position = Vector2(unit.global_position.x / minimap.game_map.map_size.x * minimap.size.x, unit.global_position.z / minimap.game_map.map_size.y * minimap.size.y)
+		if unit.is_selected:
+			modulate = Color(1, 1, 0, 1)
+		else:
+			modulate = Color(1, 1, 1, 1)
+	elif building != null:
+		global_position = Vector2(building.global_position.x / minimap.game_map.map_size.x * minimap.size.x, building.global_position.z / minimap.game_map.map_size.y * minimap.size.y)
+		modulate = Color(0, 1, 0, 1)
