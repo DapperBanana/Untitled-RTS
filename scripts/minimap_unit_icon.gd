bf20c7c568a1be82d820f90174ede6383830e007
Austin L. Howard <contact@austinlhoward.com>
2026-04-25 20:04:00 -0600

    Scale unit icon based on map size

--- scripts/minimap_unit_icon.gd
+++ scripts/minimap_unit_icon.gd
@@ -4,6 +4,12 @@ extends Sprite2D
 @export var building = null
 @export var minimap = null
 
+func _ready():
+	if unit != null:
+		scale = Vector2(minimap.game_map.map_size.x / 200, minimap.game_map.map_size.y / 200)
+	elif building != null:
+		scale = Vector2(minimap.game_map.map_size.x / 200, minimap.game_map.map_size.y / 200)
+
 func _process(delta):
 	if unit != null:
 		global_position = Vector2(unit.global_position.x / minimap.game_map.map_size.x * minimap.size.x, unit.global_position.z / minimap.game_map.map_size.y * minimap.size.y)
