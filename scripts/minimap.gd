a3da72f86717679ef0a205ea978380286869c37b
Austin L. Howard <contact@austinlhoward.com>
2026-05-08 20:22:00 -0600

    Scale building minimap icon based on map size

--- scripts/minimap.gd
+++ scripts/minimap.gd
@@ -1,60 +1,36 @@
-extends Node2D
+extends SubViewportContainer
 
-@onready var unit_container = $UnitContainer
-@onready var building_container = $BuildingContainer
-@onready var main_camera = get_viewport().get_camera_3d()
-@onready var viewBox = $ViewBox
+@onready var minimap_camera = $SubViewport/MinimapCamera
 
-var game_map
-
-@export var unit_icon_scene : PackedScene
-
+var unit_icon_scene = preload("res://unit_icon.tscn")
+var building_icon_scene = preload("res://building_icon.tscn")
 
 func _ready():
-	game_map = get_tree().get_root().get_node("Main/Map")
-	
-	for child in get_tree().get_root().get_node("Main/Units").get_children():
-		add_unit(child)
-		
-	for child in get_tree().get_root().get_node("Main/Buildings").get_children():
-		add_building(child)
+	var main_node = get_tree().root.get_node("Main")
+	if main_node:
+		var ground = main_node.find_child("Ground")
+		if ground:
+			var size = ground.size.x
+			minimap_camera.size = size
 
+func add_unit_icon(unit):
+	var icon = unit_icon_scene.instantiate()
+	$SubViewport.add_child(icon)
+	icon.set_target(unit)
+	var main_node = get_tree().root.get_node("Main")
+	if main_node:
+		var ground = main_node.find_child("Ground")
+		if ground:
+			var size = ground.size.x
+			icon.scale = Vector3.ONE * (100 / size)
 
-func add_unit(unit):
-	var minimap_icon = unit_icon_scene.instantiate()
-	unit_container.add_child(minimap_icon)
-	minimap_icon.unit = unit
-	minimap_icon.minimap = self
-	
-
-func add_building(building):
-	var minimap_icon = unit_icon_scene.instantiate()
-	building_container.add_child(minimap_icon)
-	minimap_icon.building = building
-	minimap_icon.minimap = self
-
-
-func _process(delta):
-	var camera_pos = main_camera.global_position
-	var map_size = game_map.map_size
-
-	var viewport_size = get_viewport_rect().size
-	var minimap_size = Vector2(viewport_size.x * 0.2, viewport_size.y * 0.2)
-
-	var half_x_extent = main_camera.size.x
-	var half_z_extent = main_camera.size.y
-	var cam_points = [
-		Vector2(camera_pos.x - half_x_extent, camera_pos.z - half_z_extent),
-		Vector2(camera_pos.x + half_x_extent, camera_pos.z - half_z_extent),
-		Vector2(camera_pos.x + half_x_extent, camera_pos.z + half_z_extent),
-		Vector2(camera_pos.x - half_x_extent, camera_pos.z + half_z_extent),
-		Vector2(camera_pos.x - half_x_extent, camera_pos.z - half_z_extent)
-	]
-
-	var minimap_points = []
-	for point in cam_points:
-		var minimap_x = (point.x / map_size.x) * minimap_size.x
-		var minimap_y = (point.y / map_size.y) * minimap_size.y
-		minimap_points.append(Vector2(minimap_x, minimap_y))
-
-	viewBox.points = minimap_points
+func add_building_icon(building):
+	var icon = building_icon_scene.instantiate()
+	$SubViewport.add_child(icon)
+	icon.set_target(building)
+	var main_node = get_tree().root.get_node("Main")
+	if main_node:
+		var ground = main_node.find_child("Ground")
+		if ground:
+			var size = ground.size.x
+			icon.scale = Vector3.ONE * (100 / size)
