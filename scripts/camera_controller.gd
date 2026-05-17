a0496336c452a09a480f8b2e16e62bc112a0e82e
Austin L. Howard <contact@austinlhoward.com>
2026-05-16 21:47:00 -0600

    Fix camera panning input; use get_vector

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -3,17 +3,6 @@ extends Camera3D
 @export var pan_speed = 20.0
 
 func _process(delta):
-	var input_vector = Vector3.ZERO
+	var input_vector = Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
 
-	if Input.is_action_pressed("move_forward"):
-		input_vector.z -= 1
-	if Input.is_action_pressed("move_backward"):
-		input_vector.z += 1
-	if Input.is_action_pressed("move_left"):
-		input_vector.x -= 1
-	if Input.is_action_pressed("move_right"):
-		input_vector.x += 1
-
-	input_vector = input_vector.normalized()
-
-	position += transform.basis * input_vector * pan_speed * delta
+	position += transform.basis * Vector3(input_vector.x, 0, input_vector.y) * pan_speed * delta
